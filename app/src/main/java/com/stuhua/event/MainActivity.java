580bb0c0ea93bb2dda0d4ea6ae4894909157200f
stuhua <850360626@qq.com>
2017-03-29 13:44:52 +0800

    test

--- app/src/main/java/com/stuhua/event/MainActivity.java
+++ app/src/main/java/com/stuhua/event/MainActivity.java
@@ -2,12 +2,24 @@ package com.stuhua.event;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView mTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mTv = getViewByID(R.id.tv);
+    }
+
+    public <T extends View> T getViewByID(int id) {
+        return (T) findViewById(id);
+    }
+
+    public void myEvent(View v) {
+
     }
 }
