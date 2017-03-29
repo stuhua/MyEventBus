98ede1cbd408a3f69e3da2b518e8744d85fee525
stuhua <850360626@qq.com>
2017-03-29 13:54:24 +0800

    add eventbus

--- app/src/main/java/com/stuhua/event/MainActivity.java
+++ app/src/main/java/com/stuhua/event/MainActivity.java
@@ -4,6 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
 
 public class MainActivity extends AppCompatActivity {
     private TextView mTv;
@@ -13,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mTv = getViewByID(R.id.tv);
+        EventBus.getDefault().register(this);
     }
 
     public <T extends View> T getViewByID(int id) {
@@ -20,6 +25,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void myEvent(View v) {
+        EventBus.getDefault().post(new MsgEvent("Hello EventBus!"));
+    }
 
+    @Subscribe
+    public void onMessage(MsgEvent event) {
+        mTv.setText(event.getMsg().toString());
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        EventBus.getDefault().unregister(this);
     }
 }
---
+++ app/src/main/java/com/stuhua/event/MsgEvent.java
@@ -0,0 +1,23 @@
+package com.stuhua.event;
+
+/**
+ * Created by Administrator on 2017/3/29.
+ */
+
+public class MsgEvent {
+    String msg;
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public MsgEvent(String msg) {
+        this.msg = msg;
+    }
+
+
+}
